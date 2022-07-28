59298ce3ba7ebd1766c8fdc7a467222278a2f5f8
Andreas Matheus <am@secure-dimensions.de>
2022-07-28 18:10:05 +0200

    POST and PUT added

--- pycsw/ogc/api/oapi.py
+++ pycsw/ogc/api/oapi.py
@@ -73,6 +73,67 @@ def gen_oapi(config, oapi_filepath):
         'style': 'form',
         'explode': False
     }
+    oapi['components']['parameters']['fDCS'] = {
+        'name': 'f',
+        'in': 'query',
+        'description': 'Optional output formats',
+        'required': False,
+        'schema': {
+            'type': 'string',
+            'enum': ['json', 'html', 'jose', 'dcs+geo'],
+            'default': 'json'
+        },
+        'style': 'form',
+        'explode': False
+    }
+    oapi['components']['parameters']['keyChallenge'] = {
+        'name': 'key_challenge',
+        'in': 'query',
+        'description': 'PIN for creating a DEK',
+        'required': False,
+        'schema': {
+            'type': 'string'
+        },
+        'style': 'form',
+        'explode': False
+    }
+    oapi['components']['parameters']['keyChallengeMethod'] = {
+        'name': 'key_challenge_method',
+        'in': 'query',
+        'description': 'method used to produce key_challenge according to RFC 7636',
+        'required': False,
+        'schema': {
+            'type': 'string',
+            'enum': ['plain', 'S256'],
+            'default': 'plain'
+        },
+        'style': 'form',
+        'explode': False
+    }
+    oapi['components']['parameters']['publicKeys'] = {
+        'name': 'public_keys',
+        'in': 'query',
+        'description': 'a JSON array of RFC 7515 compliant JWK (JSON Web Key)',
+        'required': False,
+        'schema': {
+            'type': 'string',
+            'example': '[{"kid":"LJS","kty":"RSA","n":"23ESdb-5g5QinXwY7QCVNBz5BF_xpZbIxPzwNS7ZWPjfuX9KtMGUbxpw-QmIuUd02k3VbHH95NppDFnzeBoJz9fs8_iBl_VHy6ASYGpbusQ0bHYacpl-cptURQWl7CFvVjkJ7uZtKL-UNKG8cbSG7LT24Ltmk_5Wf6hBPqd_0ml4ZuAq0P_D37FEUXl0NBue6iIGoc6RiZqV0SHqUc4TDMfOxaO_x6Zdy6onjRSgCh_Phs_PWucvmTBVV0nzMpnIi9T_ZCX6m9LGceyF_1ryHourb5idvrdOyozgRM5y4EyxRvfO0dOzevy8eBuliuMIJU7RzBjkQIbWpDAlawKRvQ","e":"AQAB","use":"enc"}]'
+        },
+        'style': 'form',
+        'explode': False
+    }
+    oapi['components']['parameters']['publicKey'] = {
+        'name': 'public_key',
+        'in': 'query',
+        'description': 'a RFC 7515 compliant JWK (JSON Web Key)',
+        'required': False,
+        'schema': {
+            'type': 'object',
+            'example': '{"kid":"LJS","kty":"RSA","n":"23ESdb-5g","e":"AQAB","use":"enc"}'
+        },
+        'style': 'form',
+        'explode': False
+    }
     oapi['components']['parameters']['offset'] = {
         'name': 'offset',
         'in': 'query',
@@ -226,6 +287,7 @@ def gen_oapi(config, oapi_filepath):
             'summary': 'Records search items page',
             'description': 'Records search items page',
             'operationId': 'getRecords',
+            'security': [{'BearerAuth': ["ogc"]}],
             'parameters': [
                 {'$ref': '#/components/parameters/collectionId'},
                 {'$ref': '#/components/parameters/bbox'},
@@ -236,7 +298,105 @@ def gen_oapi(config, oapi_filepath):
                 {'$ref': '#/components/parameters/externalId'},
                 {'$ref': '#/components/parameters/sortby'},
                 {'$ref': '#/components/parameters/filter'},
-                {'$ref': '#/components/parameters/f'},
+                {'$ref': '#/components/parameters/fDCS'},
+                {'$ref': '#/components/parameters/keyChallenge'},
+                {'$ref': '#/components/parameters/keyChallengeMethod'},
+                #{'$ref': '#/components/parameters/publicKeys'},
+                {'$ref': '#/components/parameters/publicKey'},
+                {'$ref': '#/components/parameters/offset'},
+                {'$ref': '#/components/parameters/vendorSpecificParameters'}
+            ],
+            'responses': {
+                '200': {
+                    '$ref': '#/components/responses/Records'
+                },
+                '400': {
+                    '$ref': '#/components/responses/InvalidParameter'
+                },
+                '401': {
+                    '$ref': '#/components/responses/TokenError'
+                },
+                '404': {
+                    '$ref': '#/components/responses/NotFound'
+                },
+                '500': {
+                    '$ref': '#/components/responses/ServerError'
+                }
+            }
+        },
+        'post': {
+            'tags': ['Data'],
+            'summary': 'Publish a Record item page',
+            'description': 'Publish a Record item',
+            'operationId': 'publishRecord',
+            'security': [{'BearerAuth': ["ogc"]}],
+            'parameters': [
+                {
+                    'in': 'path',
+                    'name': 'collectionId',
+                    'required': True,
+                    'schema': {'$ref': '#/components/parameters/collectionId'}
+                }
+            ],
+            'requestBody': {
+                'description': 'Record to be published',
+                'required': True,
+                'content': {
+                    'application/xml': {
+                        'schema': {
+                            'type': 'string'
+                        }
+                    },
+                    'application/json': {
+                        'schema': {
+                            '$ref': '#/components/schemas/recordGeoJSON'
+                        }
+                    },
+                    'application/jose': {
+                        'schema': {
+                            'type': 'string'
+                        }
+                    }
+                }
+            },
+            'responses': {
+                '201': {
+                    'description': 'Created',
+                    'headers': {
+                        'Location': {
+                             'description': 'URI to the created Record'
+                        }
+                    }
+                },
+                '401': {
+                    'description': 'Authorization information is missing or invalid.'
+                },
+                '500': {
+                    '$ref': '#/components/responses/ServerError'
+                }
+            }
+        }
+    }
+
+    oapi['paths']['/collections/{collectionId}/items'] = path
+
+    path2 =  {
+        'get': {
+            'tags': ['Data'],
+            'summary': 'Records search items page',
+            'description': 'Records search items page',
+            'operationId': 'getRecords',
+            'security': [{'BearerAuth': ["ogc"]}],
+            'parameters': [
+                {'$ref': '#/components/parameters/collectionId'},
+                {'$ref': '#/components/parameters/bbox'},
+                {'$ref': '#/components/parameters/datetime'},
+                {'$ref': '#/components/parameters/limit'},
+                {'$ref': '#/components/parameters/q'},
+                {'$ref': '#/components/parameters/type'},
+                {'$ref': '#/components/parameters/externalId'},
+                {'$ref': '#/components/parameters/sortby'},
+                {'$ref': '#/components/parameters/filter'},
                 {'$ref': '#/components/parameters/offset'},
                 {'$ref': '#/components/parameters/vendorSpecificParameters'}
             ],
@@ -256,10 +416,6 @@ def gen_oapi(config, oapi_filepath):
             }
         }
     }
-
-    oapi['paths']['/collections/{collectionId}/items'] = path
-
-    path2 = deepcopy(path)
     path2['get']['operationId'] = 'searchRecords'
     oapi['paths']['/search'] = path2
 
@@ -272,17 +428,83 @@ def gen_oapi(config, oapi_filepath):
             'summary': 'Records item page',
             'description': 'Records item page',
             'operationId': 'getRecord',
+            'security': [{'BearerAuth': ["ogc"]}],
             'parameters': [
                 {'$ref': '#/components/parameters/collectionId'},
                 {'$ref': '#/components/parameters/recordId'},
-                f
+                {'$ref': '#/components/parameters/fDCS'},
+                {'$ref': '#/components/parameters/keyChallenge'},
+                {'$ref': '#/components/parameters/keyChallengeMethod'},
+                #{'$ref': '#/components/parameters/publicKeys'},
+                {'$ref': '#/components/parameters/publicKey'}
             ],
             'responses': {
                 '200': {
                     '$ref': '#/components/responses/Record'
                 },
+                '401': {
+                    'description': 'Authorization information is missing or invalid.'
+                },
                 '404': {
                     '$ref': '#/components/responses/NotFound'
+                },
+                '500': {
+                    '$ref': '#/components/responses/ServerError'
+                }
+            }
+        },
+        'put': {
+            'tags': ['Data'],
+            'summary': 'Publish a Record item page',
+            'description': 'Publish a Record item',
+            'operationId': 'putRecord',
+            'security': [{'BearerAuth': ["ogc"]}],
+            'parameters': [
+                {
+                    'in': 'path',
+                    'name': 'collectionId',
+                    'required': True,
+                    'schema': {'$ref': '#/components/parameters/collectionId'}
+                },
+                {
+                    'in': 'path',
+                    'name': 'recordId',
+                    'required': True,
+                    'schema': {'$ref': '#/components/parameters/recordId'}
+                }
+            ],
+            'requestBody': {
+                'description': 'Record to be published',
+                'required': True,
+                'content': {
+                    'application/xml': {
+                        'schema': {
+                            'type': 'string'
+                        }
+                    },
+                    'application/json': {
+                        'schema': {
+                            '$ref': '#/components/schemas/recordGeoJSON'
+                        }
+                    },
+                    'application/jose': {
+                        'schema': {
+                            'type': 'string'
+                        }
+                    }
+                }
+            },
+            'responses': {
+                '201': {
+                    'description': 'Created',
+                    'headers': {
+                        'Location': {
+                             'description': 'URI to the created Record'
+                        }
+                    }
+                },
+                '401': {
+                     'description': 'Authorization information is missing or invalid.'
                 },
                 '500': {
                     '$ref': '#/components/responses/ServerError'
